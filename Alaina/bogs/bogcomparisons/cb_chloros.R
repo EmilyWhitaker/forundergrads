04a5fe327c047a45d01a66a25b96b1b0fea277a1
EmilyWhitaker <ewhitaker524@gmail.com>
2019-04-10 16:40:40 -0500

    updates

--- Alaina/bogs/bogcomparisons/cb_chloros.R
+++ Alaina/bogs/bogcomparisons/cb_chloros.R
@@ -1,3 +1,12 @@
 library(tidyverse)
 library(lubridate)
 
+CB_chloro <- 
+  CB_chloro %>%
+  separate(sampledate, c("month", "day", "year"), sep= "/", extra = "merge")
+
+glimpse(CB_chloro)
+
+ggplot(data = CB_chloro, mapping = aes(x = year, y = chlor), color= blue)
+ggplot(data = CB_chloro, mapping = aes(x = chlor))
+ggplot(data = CB_chloro, aes(x = year, y=chlor, color = "blue"))
