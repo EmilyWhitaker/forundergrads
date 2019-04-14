2e83bf0da492af399cace2181fbbf5b41e7a0674
EmilyWhitaker <ewhitaker524@gmail.com>
2019-04-13 19:07:33 -0500

    new files

---
+++ Alaina/lakes/crystalchloros.R
@@ -0,0 +1,73 @@
+library(tidyverse)
+library(lubridate)
+library(ggplot2)
+
+CR_chloro <-
+  CR_chloro %>%
+  mutate(
+    dpth= cut(depth,
+              breaks =  c(-Inf, 4, 14, Inf),
+              labels =  c("epi", "meta", "hypo") 
+    )
+  )
+
+
+
+CR_chloro_date <- 
+  CR_chloro %>%
+  separate(sampledate, c("month", "day", "year"), sep= "/", extra = "merge")
+
+
+CR_chloro_date <-
+  CR_chloro_date %>%
+  mutate(
+    month = as.numeric(month)
+  )
+
+
+## month <- factor(month, levels =month)
+
+#ggplot(data = SL_chloro_date, mapping = aes(x = month, y = chlor)) + 
+#  geom_point(aes(color= dpth)) +
+#  facet_wrap("dpth")
+
+#ggplot(data = SL_chloro_date, mapping = aes(x = month, y = chlor)) + 
+#  geom_point(aes(color= dpth)) +
+#  facet_wrap("dpth")+
+#  ggtitle("Sparkling Monthly Chloros")
+
+cmc <- ggplot(data = CR_chloro_date, mapping = aes(x = month, y = chlor)) + 
+  geom_point(aes(color= dpth)) +
+  facet_grid(dpth ~ .)+
+  ggtitle("Crystal Lake Monthly Chloros")
+
+cmc
+
+cmc + ylim(-10, 75)
+
+
+
+
+#ggplot(data = SL_chloro_date, mapping = aes(x = month, y = chlor, fill= chlor)) + 
+#  geom_boxplot(aes(color= dpth)) +
+#  facet_wrap("dpth")+
+#  ggtitle("Sparkling Monthly Chloros")+
+
+
+
+#ggplot(data = CL_o2_light, mapping = aes(x = o2, y = light)) + 
+#  geom_point(aes(color= dpth))
+
+
+
+
+
+#SL_chloro
+#SL_02light
+#combo <- merge( SL_02light, SL_chloro, by= "sampledate")
+#combo<- 
+#  comborematered %>%
+#  mutate(
+
+#  )
+
